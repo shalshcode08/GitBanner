8ccaed4bf0779c9d0d68dd9c3ba5726b0ebe0349
shalshcode08 <somushrestha448@gmail.com>
2026-03-06 16:22:43 +0530

    feat: added vercel analytics

--- GIT_BANNER_V2/src/main.tsx
+++ GIT_BANNER_V2/src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 import { ThemeProvider } from "./context/ThemeProvider.tsx";
 import ErrorBoundary from "./components/ErrorBoundary.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -12,6 +13,7 @@ createRoot(document.getElementById("root")!).render(
       <BrowserRouter>
         <ThemeProvider>
           <App />
+          <Analytics />
         </ThemeProvider>
       </BrowserRouter>
     </ErrorBoundary>
